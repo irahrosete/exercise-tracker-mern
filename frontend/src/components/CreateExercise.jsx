4773aca053cfaaac58238fb27a70925065ff165d
irah rosete <irah.rosete@outlook.com>
2021-08-28 01:15:52 +1000

    create react app and components

---
+++ frontend/src/components/CreateExercise.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CreateExercise = () => {
+  return <div>Create Exercise</div>
+}
+
+export default CreateExercise
---
+++ frontend/src/components/EditExercise.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EditExercise = () => {
+  return <div>Edit Exercise</div>
+}
+
+export default EditExercise
